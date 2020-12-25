a0d5f4476e19839c3bb45810afdfb4c08a635993
Mathies Föls <mathiesfoels@gmail.com>
2020-12-25 18:59:10 +0100

    Layout

--- HexBoxPropertyGridSample/EditorTest/HexEditor.Designer.cs
+++ HexBoxPropertyGridSample/EditorTest/HexEditor.Designer.cs
@@ -38,7 +38,7 @@ namespace EditorTest
             // 
             this.cancelButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.cancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.cancelButton.Location = new System.Drawing.Point(713, 415);
+            this.cancelButton.Location = new System.Drawing.Point(567, 375);
             this.cancelButton.Name = "cancelButton";
             this.cancelButton.Size = new System.Drawing.Size(75, 23);
             this.cancelButton.TabIndex = 0;
@@ -49,7 +49,7 @@ namespace EditorTest
             // 
             this.okButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.okButton.DialogResult = System.Windows.Forms.DialogResult.OK;
-            this.okButton.Location = new System.Drawing.Point(632, 415);
+            this.okButton.Location = new System.Drawing.Point(486, 375);
             this.okButton.Name = "okButton";
             this.okButton.Size = new System.Drawing.Size(75, 23);
             this.okButton.TabIndex = 1;
@@ -64,19 +64,19 @@ namespace EditorTest
             | System.Windows.Forms.AnchorStyles.Right)));
             this.hexBox1.ColumnInfoVisible = true;
             this.hexBox1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.hexBox1.LineInfoVisible = true;
             this.hexBox1.Location = new System.Drawing.Point(12, 12);
             this.hexBox1.Name = "hexBox1";
             this.hexBox1.ShadowSelectionColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(60)))), ((int)(((byte)(188)))), ((int)(((byte)(255)))));
-            this.hexBox1.Size = new System.Drawing.Size(776, 397);
+            this.hexBox1.Size = new System.Drawing.Size(630, 357);
             this.hexBox1.StringViewVisible = true;
             this.hexBox1.TabIndex = 2;
-            this.hexBox1.UseFixedBytesPerLine = true;
             // 
             // HexEditor
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(654, 410);
             this.Controls.Add(this.hexBox1);
             this.Controls.Add(this.okButton);
             this.Controls.Add(this.cancelButton);
